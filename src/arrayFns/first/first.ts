4596fddffa5c1ac475ffa02a087e1f309353d21e
joao-arthur <joao.lothamer@gmail.com>
2022-12-01 20:00:50 -0300

    refactor: remove unnecessary array validations

--- src/arrayFns/first/first.ts
+++ src/arrayFns/first/first.ts
@@ -1,5 +1,3 @@
 export function first<T>(arr: readonly T[]): T | undefined {
-    if (!arr.length)
-        return undefined;
     return arr[0];
 }
--- src/arrayFns/last/last.ts
+++ src/arrayFns/last/last.ts
@@ -1,5 +1,3 @@
 export function last<T>(arr: readonly T[]): T | undefined {
-    if (!arr.length)
-        return undefined;
     return arr[arr.length - 1];
 }
