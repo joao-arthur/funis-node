ae9c3dcb9270b625d6279e75facd2d9d8cac8c12
joao-arthur <joao.lothamer@gmail.com>
2022-12-01 21:57:18 -0300

    feat: add latest functions to numberfns module

--- src/numberFns/numberFns.ts
+++ src/numberFns/numberFns.ts
@@ -1,9 +1,13 @@
 import { clamp } from './clamp';
 import { isValid } from './isValid';
 import { range } from './range';
+import { parsePrefixed } from './parsePrefixed';
+import { parseSuffixed } from './parseSuffixed';
 
 export const numberFns = {
     clamp,
     isValid,
     range,
+    parsePrefixed,
+    parseSuffixed,
 } as const;
