4fecf9ed318ccc07384af724c2ac8feb3bada95e
joao-arthur <joao.lothamer@gmail.com>
2022-12-02 15:08:26 -0300

    feat: plain object type

--- src/objectFns/objectFns.ts
+++ src/objectFns/objectFns.ts
@@ -1,3 +1,11 @@
-export const objectFns = {
+import { prefix } from './prefix';
+import { suffix } from './suffix';
+import { serialize } from './serialize';
+import { serializesToSame } from './serializesToSame';
 
+export const objectFns = {
+    prefix,
+    suffix,
+    serialize,
+    serializesToSame,
 } as const;
---
+++ src/types/plainObject.ts
@@ -0,0 +1,3 @@
+export type plainObject<T = unknown> = {
+    [key: string | number]: T;
+}
