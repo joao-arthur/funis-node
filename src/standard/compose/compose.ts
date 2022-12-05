10a192a3df9f883b0544dcab4c4dd85619b8c0dc
joao-arthur <joao.lothamer@gmail.com>
2022-12-04 23:09:50 -0300

    feat: pipe and compose implementation

---
+++ src/standard/compose/compose.ts
@@ -0,0 +1,405 @@
+type unknownFn = (arg: unknown) => unknown;
+
+export function compose<A, B>(
+    fnA: (a: A) => B,
+): (initialValue: A) => B;
+
+export function compose<A, B, C>(
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => C;
+
+export function compose<A, B, C, D>(
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => D;
+
+export function compose<A, B, C, D, E>(
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => E;
+
+export function compose<A, B, C, D, E, F>(
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => F;
+
+export function compose<A, B, C, D, E, F, G>(
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => G;
+
+export function compose<A, B, C, D, E, F, G, H>(
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => H;
+
+export function compose<A, B, C, D, E, F, G, H, I>(
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => I;
+
+export function compose<A, B, C, D, E, F, G, H, I, J>(
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => J;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K>(
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => K;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L>(
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => L;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M>(
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => M;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => N;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => O;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => P;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q>(
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => Q;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R>(
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => R;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S>(
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => S;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T>(
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => T;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U>(
+    fnT: (t: T) => U,
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => U;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V>(
+    fnU: (u: U) => V,
+    fnT: (t: T) => U,
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => V;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W>(
+    fnV: (v: V) => W,
+    fnU: (u: U) => V,
+    fnT: (t: T) => U,
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => W;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X>(
+    fnW: (w: W) => X,
+    fnV: (v: V) => W,
+    fnU: (u: U) => V,
+    fnT: (t: T) => U,
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => X;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y>(
+    fnX: (x: X) => Y,
+    fnW: (w: W) => X,
+    fnV: (v: V) => W,
+    fnU: (u: U) => V,
+    fnT: (t: T) => U,
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => Y;
+
+export function compose<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z>(
+    fnY: (y: Y) => Z,
+    fnX: (x: X) => Y,
+    fnW: (w: W) => X,
+    fnV: (v: V) => W,
+    fnU: (u: U) => V,
+    fnT: (t: T) => U,
+    fnS: (s: S) => T,
+    fnR: (r: R) => S,
+    fnQ: (q: Q) => R,
+    fnP: (p: P) => Q,
+    fnO: (o: O) => P,
+    fnN: (n: N) => O,
+    fnM: (m: M) => N,
+    fnL: (l: L) => M,
+    fnK: (k: K) => L,
+    fnJ: (j: J) => K,
+    fnI: (i: I) => J,
+    fnH: (h: H) => I,
+    fnG: (g: G) => H,
+    fnF: (f: F) => G,
+    fnE: (e: E) => F,
+    fnD: (d: D) => E,
+    fnC: (c: C) => D,
+    fnB: (b: B) => C,
+    fnA: (a: A) => B,
+): (initialValue: A) => Z;
+
+export function compose(...fns: unknownFn[]): unknownFn {
+    return initialParam => fns.reduceRight((curr, fn) => fn(curr), initialParam);
+}
---
+++ src/standard/pipe/pipe.ts
@@ -0,0 +1,405 @@
+type unknownFn = (arg: unknown) => unknown;
+
+export function pipe<A, B>(
+    fnA: (a: A) => B,
+): (initialValue: A) => B;
+
+export function pipe<A, B, C>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+): (initialValue: A) => C;
+
+export function pipe<A, B, C, D>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+): (initialValue: A) => D;
+
+export function pipe<A, B, C, D, E>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+): (initialValue: A) => E;
+
+export function pipe<A, B, C, D, E, F>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+): (initialValue: A) => F;
+
+export function pipe<A, B, C, D, E, F, G>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+): (initialValue: A) => G;
+
+export function pipe<A, B, C, D, E, F, G, H>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+): (initialValue: A) => H;
+
+export function pipe<A, B, C, D, E, F, G, H, I>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+): (initialValue: A) => I;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+): (initialValue: A) => J;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+): (initialValue: A) => K;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+): (initialValue: A) => L;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+): (initialValue: A) => M;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+): (initialValue: A) => N;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+): (initialValue: A) => O;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+): (initialValue: A) => P;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+): (initialValue: A) => Q;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+): (initialValue: A) => R;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+): (initialValue: A) => S;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+): (initialValue: A) => T;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+    fnT: (t: T) => U,
+): (initialValue: A) => U;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+    fnT: (t: T) => U,
+    fnU: (u: U) => V,
+): (initialValue: A) => V;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+    fnT: (t: T) => U,
+    fnU: (u: U) => V,
+    fnV: (v: V) => W,
+): (initialValue: A) => W;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+    fnT: (t: T) => U,
+    fnU: (u: U) => V,
+    fnV: (v: V) => W,
+    fnW: (w: W) => X,
+): (initialValue: A) => X;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+    fnT: (t: T) => U,
+    fnU: (u: U) => V,
+    fnV: (v: V) => W,
+    fnW: (w: W) => X,
+    fnX: (x: X) => Y,
+): (initialValue: A) => Y;
+
+export function pipe<A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z>(
+    fnA: (a: A) => B,
+    fnB: (b: B) => C,
+    fnC: (c: C) => D,
+    fnD: (d: D) => E,
+    fnE: (e: E) => F,
+    fnF: (f: F) => G,
+    fnG: (g: G) => H,
+    fnH: (h: H) => I,
+    fnI: (i: I) => J,
+    fnJ: (j: J) => K,
+    fnK: (k: K) => L,
+    fnL: (l: L) => M,
+    fnM: (m: M) => N,
+    fnN: (n: N) => O,
+    fnO: (o: O) => P,
+    fnP: (p: P) => Q,
+    fnQ: (q: Q) => R,
+    fnR: (r: R) => S,
+    fnS: (s: S) => T,
+    fnT: (t: T) => U,
+    fnU: (u: U) => V,
+    fnV: (v: V) => W,
+    fnW: (w: W) => X,
+    fnX: (x: X) => Y,
+    fnY: (y: Y) => Z,
+): (initialValue: A) => Z;
+
+export function pipe(...fns: unknownFn[]): unknownFn {
+    return initialParam => fns.reduce((curr, fn) => fn(curr), initialParam);
+}
