c87dff06c986d5c1efe34f600c72d457df5d4b89
joao-arthur <joao.lothamer@gmail.com>
2022-12-08 22:12:33 -0300

    publish: 0.1.3

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { arrays } from './arrays/arrays.js';
+export { maps } from './maps/maps.js';
+export { numbers } from './numbers/numbers.js';
+export { objects } from './objects/objects.js';
+export { promises } from './promises/promises.js';
+export { standard } from './standard/standard.js';
+export { strings } from './strings/strings.js';
