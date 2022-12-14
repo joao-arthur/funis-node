e6d4d62e95a89dad7d6a9cb84a611be13f2a4de2
joao-arthur <joao.lothamer@gmail.com>
2022-12-14 19:09:07 -0300

    refactor: remove unused as const

--- src/objects/serialize/serialize.test.ts
+++ src/objects/serialize/serialize.test.ts
@@ -11,7 +11,7 @@ describe('serialize', () => {
             alive: true,
             birthday: new Date(1942, 5, 18),
             unique: Symbol('Paul'),
-        } as const;
+        };
 
         expect(serialize(object)).toEqual(
             '{ name: Paul, numberOfBands: 4, bigNumberOfBands: 4, alive: true, birthday: 1942-06-18T03:00:00.000Z, unique: Symbol(Paul) }',
@@ -20,9 +20,9 @@ describe('serialize', () => {
 
     it('should remove the function entries', () => {
         const object = {
-            sayHi: () => 'hi, my name is Eminem',
-            sayGoodbye: () => 'bye, bye, baby',
-        } as const;
+            sayHi: (): string => 'hi, my name is Eminem',
+            sayGoodbye: (): string => 'bye, bye, baby',
+        };
 
         expect(serialize(object)).toEqual('{ }');
     });
@@ -32,7 +32,7 @@ describe('serialize', () => {
             empty: [],
             oneValue: [1],
             multiValue: [true, false, 'zaphod', 42n],
-        } as const;
+        };
 
         expect(serialize(object)).toEqual(
             '{ empty: , oneValue: 1, multiValue: true, false, zaphod, 42 }',
