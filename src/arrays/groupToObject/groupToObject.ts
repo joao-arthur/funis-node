52aa3a82b96ce1a8acfa4e262e7ed2ee25c3d6dd
joao-arthur <joao.lothamer@gmail.com>
2022-12-14 19:23:15 -0300

    feat: group to entries array method

--- src/arrays/groupToObject/groupToObject.ts
+++ src/arrays/groupToObject/groupToObject.ts
@@ -1,12 +1,8 @@
-import { pipe } from '../../standard/pipe/pipe.js';
 import { plainObject } from '../../types/plainObject.js';
-import { unique } from '../unique/unique.js';
+import { groupToEntries } from '../groupToEntries/groupToEntries.js';
 
-export function groupToObject<K, V>(arr: readonly V[], cb: (item: V) => K): plainObject<V> {
-    return pipe(
-        (items: readonly V[]) => items.map(cb),
-        unique,
-        keys => keys.map(key => [key, arr.filter(item => cb(item) === key)] as const),
-        groups => Object.fromEntries(groups),
-    )(arr);
+export function groupToObject<K, V>(
+    arr: readonly V[], cb: (item: V) => K,
+): plainObject<V> {
+    return Object.fromEntries(groupToEntries(arr, cb));
 }
