dd0b482de7bfa63035efeb77e55be6238402e244
joao-arthur <joao.lothamer@gmail.com>
2022-12-14 22:20:58 -0300

    docs: setup docs structure

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,47 @@
+export default {
+    title: 'funis',
+    description: 'Optionated type-safe utility library',
+    themeConfig: {
+        sidebar: [
+            {
+                text: 'arrays',
+                collapsible: true,
+                items: [
+                    { text: 'first', link: '/arrays/first' },
+                    { text: 'last', link: '/arrays/last' },
+                    { text: 'sort', link: '/arrays/sort' },
+                    { text: 'unique', link: '/arrays/unique' },
+                    { text: 'once', link: '/arrays/once' },
+                    { text: 'disjoint', link: '/arrays/disjoint' },
+                    { text: 'intersect', link: '/arrays/intersect' },
+                    { text: 'groupToEntries', link: '/arrays/group-to-entries' },
+                    { text: 'groupToArray', link: '/arrays/group-to-array' },
+                    { text: 'groupToMap', link: '/arrays/group-to-map' },
+                    { text: 'groupToObject', link: '/arrays/group-to-object' },
+                ]
+            },
+            {
+                text: 'maps',
+                collapsible: true,
+                items: [
+                    { text: 'first', link: '/maps/first' },
+                    { text: 'last', link: '/maps/last' },
+                    { text: 'entries', link: '/maps/entries' },
+                    { text: 'keys', link: '/maps/keys' },
+                    { text: 'values', link: '/maps/values' },
+                ]
+            },
+            {
+                text: 'numbers',
+                collapsible: true,
+                items: [
+                    { text: 'clamp', link: '/numbers/clamp' },
+                    { text: 'isValid', link: '/numbers/is-valid' },
+                    { text: 'parse', link: '/numbers/parse' },
+                    { text: 'range', link: '/numbers/range' },
+                    { text: 'lazyRange', link: '/numbers/lazy-range' },
+                ]
+            },
+        ]
+    }
+};
