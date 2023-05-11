2bff118359090cf84c66c7d906d62756990db5d9
joao-arthur <joao.lothamer@gmail.com>
2023-05-11 12:58:10 -0300

    refactor: removeAccentuation

--- src/str/removeAccentuation/removeAccentuation.ts
+++ src/str/removeAccentuation/removeAccentuation.ts
@@ -17,5 +17,6 @@ import { accents } from "../internal/accents.js";
  */
 export function removeAccentuation(str: string): string {
     return Array.from(str)
-        .map((letter) => accents.has(letter) ? "" : letter).join("");
+        .filter((letter) => !accents.has(letter))
+        .join("");
 }
